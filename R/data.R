2345ecf29f98526366b6e5ae0fa8e8dab52e50ee
diannamcallister <dd.mcallister@hotmail.ca>
2020-11-14 16:16:16 -0500

    added fake data to make sure data import will work correctly

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' DNA sequence to Check For CpGIslands
+#'
+#' A string of nucleotides that will be used to check for CpGIslands
+#'
+#' @source fake source for now
+#'
+#' @format a string of lowercase characters that only include 'g', 'c', 't', 'a'
+#'
+#' @examples
+#' \dontrun{
+#'  PossibleCpGIslands
+#' }
+"PossibleCpGIslands"
+
+#' Two DNA sequences to see before and after bisulfite conversion
+#'
+#' Two DNA sequences. One is the original sequence, the other is the sequence
+#'     after bisulfite conversion
+#'
+#' @source fake source for now
+#'
+#' @format{
+#' Two DNA sequences:
+#' \describe{
+#' \item{Original DNA Sequence}{The DNA sequence before any modifications were made.}
+#' \item{Post-Bisulfite Conversion DNA Sequence}{The DNA sequence after bisulfite conversion.}
+#' }
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'  MethylationObservation
+#' }
+"MethylationObservation"
